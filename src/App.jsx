e303ad47954e11815c9d2541c0c53efff35224cf
Jurica <jvidacak123@gmail.com>
2024-03-11 21:14:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom/cjs/react-router-dom.min";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data"
+import BookInfo from "./pages/BookInfo";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
