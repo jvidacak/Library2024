89d52989686e0ba556abe9e86ec1b8f7f23f0049
Jurica <jvidacak123@gmail.com>
2024-03-12 20:16:14 +0100

    last commit

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,14 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
 import Book from "../components/ui/Book";
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart, cart }) => {
   const { id } = useParams();
   const book = books.find((book) => +book.id === +id);
-  console.log(book);
+
+
+  function addBookToCart(book) {
+    addToCart(book)
+  }
+
+  function bookExistsOnCart() {
+     return cart.find(book => book.id === +id);
+  }
 
   return (
     <div id="books__body">
@@ -51,7 +59,13 @@ const BookInfo = ({ books }) => {
                     eligendi harum obcaecati illum rem et!
                   </p>
                 </div>
-                <button className="btn">Add to cart</button>
+                {bookExistsOnCart() ? (
+                <Link to={'/cart'} className="book__link">
+                 <button className="btn">Checkout</button>
+                 </Link>
+                ) : (
+                <button className="btn" onClick={() => addBookToCart(book)}>Add to cart</button>
+                ) }
               </div>
             </div>
           </div>
